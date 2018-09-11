53d9ce593f24df4f148d58e15f197aaa2196a82f
ani11056 <39136702+ani11056@users.noreply.github.com>
2018-09-10 21:05:42 -0400

    Update App.java

--- src/main/java/com/training/App.java
+++ src/main/java/com/training/App.java
@@ -14,7 +14,7 @@ public class App
     public static void main( String[] args ) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         server.createContext("/", new MyHandler());
-        server.setExecutor(null); // creates a default executor
+        server.setExecutor(null); // creates a default executor program
         server.start();
     }
     static class MyHandler implements HttpHandler {
